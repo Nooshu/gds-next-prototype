8133c0a0a18c9f1e1dd773f24fb4fb522a15029d
Andrew Ajube <andrew.ajube@hmcts.net>
2025-10-13 10:41:19 +0100

    chore: cleanup next config; add reduced motion CSS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    optimizePackageImports: ["govuk-frontend", "@ministryofjustice/frontend"],
 };
 
 export default nextConfig;
