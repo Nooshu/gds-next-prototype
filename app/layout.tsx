54c4906ebda3e8d461fda1a09da24dd6f5983eac
Matt Hobbs <matthew.hobbs@hmcts.net>
2025-12-18 10:13:25 +0000

    Add robots meta tag to layout and update .npmrc and .nvmrc files

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,19 @@ export const metadata: Metadata = {
     },
     description:
         "Prototype of FaCT screens using Next.js SSR and GOV.UK Frontend",
+    robots: {
+        index: false,
+        follow: false,
+        nocache: true,
+        googleBot: {
+            index: false,
+            follow: false,
+            noimageindex: true,
+            "max-video-preview": -1,
+            "max-image-preview": "none",
+            "max-snippet": -1,
+        },
+    },
 };
 
 export default function RootLayout({
